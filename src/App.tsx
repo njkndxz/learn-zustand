d065e7776535a0e85678369519494e8ff3b8d3d7
jiang <1943889113@qq.com>
2025-08-10 22:15:10 +0800

    提交代码

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { CatBox } from "./components/CatBox";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Zustand Tutorial</h1>
+      <div>
+        <CatBox />
+      </div>
+    </div>
+  );
+}
+
+export default App;
